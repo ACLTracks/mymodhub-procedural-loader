c73ac0f4fae0af9bcd49627abd4607311ced0a80
Ashes <32988148+ACLTracks@users.noreply.github.com>
2026-01-06 12:07:34 -0700

    Update Plugin.cpp

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -1,1 +1,19 @@
+#include "Plugin.h"
+#include "Log.h"
+#include "mmh/PackLoader.h"
 
+namespace mmh
+{
+	void Plugin::Init()
+	{
+		InitLogging();
+		Info("Plugin::Init begin");
+
+		// Scaffold behavior: scan packs and log what we see.
+		auto result = mmh::PackLoader::Scan();
+		Info("Pack scan complete. packs=" + std::to_string(result.packsFound) +
+		     " entries=" + std::to_string(result.entriesFound));
+
+		Info("Plugin::Init end");
+	}
+}
