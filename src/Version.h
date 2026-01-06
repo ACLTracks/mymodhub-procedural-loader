d59db5954a0b2f8d6d0b414c7fe58981bbb5043c
Ashes <32988148+ACLTracks@users.noreply.github.com>
2026-01-06 12:06:13 -0700

    Update Version.h

--- src/Version.h
+++ src/Version.h
@@ -1,1 +1,9 @@
+#pragma once
 
+namespace mmh
+{
+	inline constexpr const char* kPluginName = PLUGIN_NAME;
+	inline constexpr unsigned kVersionMajor = 0;
+	inline constexpr unsigned kVersionMinor = 1;
+	inline constexpr unsigned kVersionPatch = 0;
+}
