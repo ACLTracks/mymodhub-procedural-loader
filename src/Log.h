1bc81aa31e1da7f55681a068bef65bd8f89e09e6
Ashes <32988148+ACLTracks@users.noreply.github.com>
2026-01-06 12:06:30 -0700

    Update Log.h

--- src/Log.h
+++ src/Log.h
@@ -1,1 +1,10 @@
+#pragma once
+#include <string>
 
+namespace mmh
+{
+	void InitLogging();
+	void Info(const std::string& msg);
+	void Warn(const std::string& msg);
+	void Error(const std::string& msg);
+}
