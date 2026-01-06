52059f5bbb959b91c9b7df643a2670f4d36c6b42
Ashes <32988148+ACLTracks@users.noreply.github.com>
2026-01-06 12:11:59 -0700

    Update RaceMenuBridge.h

--- src/racemenu/RaceMenuBridge.h
+++ src/racemenu/RaceMenuBridge.h
@@ -1,1 +1,10 @@
+#include "RaceMenuBridge.h"
 
+namespace mmh
+{
+	bool RaceMenuBridge::IsAvailable()
+	{
+		// TODO: Detect RaceMenu at runtime via SKSE messaging / module check
+		return false;
+	}
+}
