252bd34f5b7b05b3ef5036707b1e5c5df7b6065f
Ashes <32988148+ACLTracks@users.noreply.github.com>
2026-01-06 12:08:53 -0700

    Update PackPaths.cpp

--- src/mmh/PackPaths.cpp
+++ src/mmh/PackPaths.cpp
@@ -1,1 +1,13 @@
+#include "PackPaths.h"
 
+namespace mmh
+{
+	std::vector<std::string> PackPaths::DefaultRoots()
+	{
+		// Planned: Data/MyModHub/ProceduralPacks
+		// Scaffold: user can drop a test folder path in later.
+		return {
+			"Data/MyModHub/ProceduralPacks"
+		};
+	}
+}
