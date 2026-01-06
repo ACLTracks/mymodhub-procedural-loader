fc3dba6b4733783e18b44c580063aca066eab8c1
Ashes <32988148+ACLTracks@users.noreply.github.com>
2026-01-06 12:09:36 -0700

    Update PackLoader.h

--- src/mmh/PackLoader.h
+++ src/mmh/PackLoader.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <cstdint>
 
+namespace mmh
+{
+	struct ScanResult
+	{
+		std::uint32_t packsFound{ 0 };
+		std::uint32_t entriesFound{ 0 };
+	};
+
+	class PackLoader
+	{
+	public:
+		static ScanResult Scan();
+	};
+}
